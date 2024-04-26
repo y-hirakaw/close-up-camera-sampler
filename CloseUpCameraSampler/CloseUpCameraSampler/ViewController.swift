d976d6353c3a371c28e5c9c862b8d49d420f65c3
y-hirakaw <130817538+y-hirakaw@users.noreply.github.com>
2024-04-27 07:59:44 +0900

    プロジェクトを追加

---
+++ CloseUpCameraSampler/CloseUpCameraSampler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CloseUpCameraSampler
+//
+//  Created by 平川裕多 on 2024/04/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
